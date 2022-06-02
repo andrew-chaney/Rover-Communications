fbfc290cecaaee3518d77180aa6f36d4dd2f8e16
Andrew Chaney <andrewchaney24@gmail.com>
2022-06-01 18:05:57 -0600

    Added header and footer as well as combined try/except statements in main_control.py. Need to add header/footer functionality on the rover-side and also confirm that these are good values for this role.

--- 900mhz/homebase/main_control.py
+++ 900mhz/homebase/main_control.py
@@ -10,7 +10,7 @@ def setup_logger(verbose=False):
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s")
-    
+
     file_handler = logging.FileHandler("transmissions.log")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -31,6 +31,12 @@ def get_controller():
 
 
 def write_to_stream(x) -> bytearray:
+    # Header and footer to show where the packet starts/stops
+    header = 100
+    footer = 101
+    x.insert(0, header)
+    x.append(footer)
+
     data = bytearray(x)
     Serial.write(data)
     time.sleep(0.05)
@@ -42,28 +48,24 @@ def main():
 
     setup_logger(verbose=True)
 
-    logger.debug("Attempting serial connection.")
     try:
+        logger.debug("Attempting serial connection.")
         Serial = serial.Serial(
             port="/dev/tty.usbserial-AB0KTHBP",
             baudrate="57600",
             timeout=.1
         )
-    except Exception as e:
-        logger.error(e)
-        sys.exit(e)
-    logger.debug("Serial connection successful.")
-    
-    logger.debug("Attempting controller connection.")
-    try:
-        get_controller()
-    except Exception as e:
-        logger.error(e)
-        sys.exit(e)
-    logger.debug("Controller connection successful.")
+        logger.debug("Serial connection successful.")
 
-    
-    packets_num = 0
+        logger.debug("Attempting controller connection.")
+        get_controller()
+        logger.debug("Controller connection successful.")
+    except Exception as e:
+        logger.error(e)
+        sys.exit(e)
+
+
+    packet_num = 0
     logger.debug("Listening for input to transmit.")
     while True:
         output = controller.read(64)
